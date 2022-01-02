1fbba1769cdc0c5ec526622848d8309b4a6aef94
Seung-wan <tmddhks0104@gmail.com>
2022-01-02 20:36:06 +0900

    feat. webpack setting

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Greeting from './components/Greeting';
+const App = () => {
+  return (
+    <>
+      <div>This is App component</div>
+      <Greeting />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Greeting.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Greeting = () => {
+  return <h1>안녕하세요, 반갑습니다.</h1>;
+};
+
+export default Greeting;
